774b22f5d380b0d80d6c1d99450a93b4416007bc
Abdul Karim <31172014+Abdulk084@users.noreply.github.com>
2019-11-13 19:23:22 +1000

    Add files via upload

---
+++ lib.sh
@@ -0,0 +1,14 @@
+conda install -c conda-forge tensorflow
+conda install -c conda-forge keras
+conda install scikit-learn
+conda install -c anaconda pandas
+conda install -c anaconda numpy 
+conda install -c anaconda scipy
+conda install -c anaconda openpyxl
+conda install -c conda-forge xlsxwriter
+conda install -c conda-forge matplotlib
+conda install -c rdkit -c mordred-descriptor mordred
+pip install padelpy
+pip install deepchem
+
+
